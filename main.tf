6ab5753c92b3f9b263c5f578e04bd59b647257b1
akshay-sudo <55108278+akshay-sudo@users.noreply.github.com>
2024-06-07 16:05:59 +0530

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,63 @@
+#Terraform block
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "4.58.0"
+    }
+  }
+}
+
+#Provider block
+provider "aws" {
+  region     = var.awsRegion
+  access_key = var.accessKey
+  secret_key = var.secretKey
+}
+
+
+variable "awsRegion" {}
+
+variable "accessKey" {}
+
+variable "secretKey" {}
+
+variable "stackName" {
+  type= string
+}
+/*
+variable "bucketName" {
+    type = string  
+}
+
+variable "accessControl" {
+  type = string
+}
+
+variable "bucketEncryption" {
+  type = string
+}
+*/
+resource "aws_cloudformation_stack" "passwd-policy" {
+  name = var.stackName
+  capabilities = ["CAPABILITY_IAM", "CAPABILITY_AUTO_EXPAND"]
+  parameters = {
+    #BucketName = var.bucketName
+    #AccessControl = var.accessControl
+    #BucketEncryption = var.bucketEncryption
+    PermissionsBoundary = " "
+    MinimumPasswordLength = 6
+    RequireUppercaseCharacters = true
+    RequireLowercaseCharacters = true
+    RequireNumbers = true
+    RequireSymbols = true
+    MaxPasswordAge = 90
+    PasswordReusePrevention = 24
+    AllowUsersToChangePassword = true
+    HardExpiry = false
+    LogsRetentionInDays = 90
+    
+  }
+
+  template_body = file("${path.module}/password-policy.yaml")
+}
