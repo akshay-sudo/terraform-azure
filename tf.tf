c6cfe84fccd7f884c8599be15bd7f5f42c7de8f4
akshay-sudo <55108278+akshay-sudo@users.noreply.github.com>
2024-06-13 19:24:02 +0530

    Create tf.tf

---
+++ tf.tf
@@ -0,0 +1,4 @@
+subscription_id="f8f3125f-2ef7-40ee-b469-86adcf397b86"
+tenant_id="ff355289-721e-4dd7-a663-afec62ab9d54"
+client_id="e6125b28-4b6e-462b-b58f-8cf4c15cbefc"
+CSq8Q~NEehEaeHQlykFB5HbGhy6in~0JZbToSbHh"
